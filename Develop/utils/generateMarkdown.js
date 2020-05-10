6f09325044740afd5d0c806b80b773a8349c343e
Drkeck <k3ckl3y.a@gmail.com>
2020-05-10 15:36:34 -0700

    added basic generation for markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.name}
 
+  ${data.description}
+
+
+  ## table of contents
+  
+  * [Installation]()
+  * [Usage]()
+  * [Credits]()
+  * [Liscense]()
+  * [Tests]()
+
+
+  ## Installation
+    
+  ${data.installation}
+
+
+  ## Usage 
+
+  ${data.usage}
+
+
+  ## License
+
+  ${data.liscense}
+
+
+  ## Credits
+
+  ${data.contributing}
+
+
+  ## tests
+
+  ${data.tests}
+
+
+  ## get in contact
+  Contact [${data.github}](https://www.github.com/${data.github}) if you have any questions or concerns
+  
+  or you can contact us by [Email](${data.email})
 `;
 }
 
